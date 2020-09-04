4f76fb7eaf2e5f1104baa89247719b2e1bb4e24c
Fredo Picazo <fredo.picazo@blackswan.com>
2020-09-04 09:07:01 -0700

    completed video 3 by creating store picker component.

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,11 @@
+// always import into your component, don't worry it won't reproduce multiple copies
+import React from 'react';
+
+// each component is going to be its own class
+class StorePicker extends React.Component {
+  render() {
+    return <p>what up bitches again!</p>
+  }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,10 @@
 // let's go!
+// import dependencies
+// never capitalize 'react'
+import React from 'react';
+// for web application
+import { render } from 'react-dom'
+// import component
+import StorePicker from "./components/StorePicker";
+// render(<p>Yoooo</p>, document.querySelector('#main'));
+render(<StorePicker />, document.querySelector('#main'));
