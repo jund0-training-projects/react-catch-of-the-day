bbee15f149ae110a50117cda6134dda72d257fdc
Fredo Picazo <fredo.picazo@blackswan.com>
2020-09-04 16:56:53 -0700

    updated app and inventory component to be able to load fish data sample.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
+
 class App extends React.Component {
   // where we need to set initial state
   // use contructor or property
@@ -24,6 +26,10 @@ class App extends React.Component {
     });
   };
 
+  loadSampleFishes = () => {
+    this.setState({fishes: sampleFishes});
+  };
+
   render() {
     return (
       <div className='catch-of-the-day'>
@@ -31,7 +37,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     )
   }
