c2fbf07b362a3a6c832b7506acbba458921251fb
Fredo Picazo <fredo.picazo@blackswan.com>
2020-09-04 12:33:21 -0700

    updated store picker to leverage helper function

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,5 +1,6 @@
 // always import into your component, don't worry it won't reproduce multiple copies
 import React from 'react';
+import { getFunName } from "../helpers";
 
 // each component is going to be its own class
 class StorePicker extends React.Component {
@@ -8,7 +9,7 @@ class StorePicker extends React.Component {
       <form className='store-selector'>
         { /*comment example it must exist within the returned element or fragment*/}
         <h2>Please Enter A Store</h2>
-        <input type='text' required placeholder='Store Name'/>
+        <input type='text' required placeholder='Store Name' defaultValue={getFunName()}/>
         <button type='submit'>Vist Store &rarr;</button>
       </form>
     )
