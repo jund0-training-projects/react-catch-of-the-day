ec27637832d1cb06c2d3d7e580a9cb59a154cb98
Fredo Picazo <fredo.picazo@blackswan.com>
2020-09-04 09:36:50 -0700

    example of react fragment.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,7 +4,15 @@ import React from 'react';
 // each component is going to be its own class
 class StorePicker extends React.Component {
   render() {
-    return <p>what up bitches again!</p>
+    return (
+      <React.Fragment>
+        <h1>hello</h1>
+        { /*comment example it must exist within the returned element or fragment*/}
+        <form className='store-selector'>
+          <h2>Please Enter A Store</h2>
+        </form>
+      </React.Fragment>
+    )
   }
 }
 
