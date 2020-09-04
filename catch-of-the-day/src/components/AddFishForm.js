091f4abf4409b9446b12755a7d4e28da5f6e56ee
Fredo Picazo <fredo.picazo@blackswan.com>
2020-09-04 16:44:17 -0700

    Updated App and Inventory to support add Fish functionality and managing state.

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+
+  createFish = (event) => {
+    event.preventDefault();
+    const fish = {
+      name: this.nameRef.current.value,
+      price: parseFloat(this.priceRef.current.value),
+      statue: this.statusRef.current.value,
+      dese: this.descRef.current.value,
+      image: this.imageRef.current.value
+    }
+    console.log('making a fish ');
+    console.log(fish);
+    // how do we get the fish to state
+    // from the app module, created an addFish method thats passed through the props
+    // of the Inventory Module
+    this.props.addFish(fish);
+  }
+
+
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+        <input name="price" ref={this.priceRef} type="text" placeholder="Price" />
+        <select name="status" ref={this.statusRef} type="text" placeholder="Status" >
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name="desc" ref={this.descRef} placeholder="Desc" ></textarea>
+        <input name="image" ref={this.imageRef} type="text" placeholder="Image" />
+        <button type="submit">+ Add Fish</button>
+      </form>
+    )
+  }
+}
+
+export default AddFishForm;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,14 +3,35 @@ import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
 class App extends React.Component {
+  // where we need to set initial state
+  // use contructor or property
+
+  state = {
+    fishes: {},
+    order: {}
+  }
+  // need to create secondary method to add fish to state
+  addFish = fish => {
+    console.log("Adding a fish!");
+    // in order to update state, you have to use react state api
+    // 1. Take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // 2. Add our new fish to that fishes
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    });
+  };
+
   render() {
     return (
-      <div class='catch-of-the-day'>
+      <div className='catch-of-the-day'>
         <div className='menu'>
           <Header tagline="Fresh Seafood Market"/>
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
