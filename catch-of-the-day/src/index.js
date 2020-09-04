15f5fd66c3feb1dcedc18696d9bb0f6e84ff1dca
Fredo Picazo <fredo.picazo@blackswan.com>
2020-09-04 09:48:07 -0700

    completed video 5 loading css into react application.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,7 @@
-// let's go!
-// import dependencies
-// never capitalize 'react'
 import React from 'react';
-// for web application
 import { render } from 'react-dom'
-// import component
 import StorePicker from "./components/StorePicker";
-// render(<p>Yoooo</p>, document.querySelector('#main'));
+import './css/style.css';
+
+
 render(<StorePicker />, document.querySelector('#main'));
