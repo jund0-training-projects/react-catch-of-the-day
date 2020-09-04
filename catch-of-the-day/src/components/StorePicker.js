1d799ba140d8af453d31560e20b1858b5f50f907
Fredo Picazo <fredo.picazo@blackswan.com>
2020-09-04 15:22:57 -0700

    updating store picker with custom methods to set up events, refs and this binding.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,12 +4,41 @@ import { getFunName } from "../helpers";
 
 // each component is going to be its own class
 class StorePicker extends React.Component {
+
+  // constructor() {
+  //   // supper will instantiate React.Component
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  myInput = React.createRef();
+
+  // goToStore(event) {
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // Never manipulate the dom
+    // 2. get the text from that input
+    // console.log(this) => null because custom method is not a property of instance of StorePicker
+    console.log(this.myInput);
+    // 3. Change the page to /store/whatever-they-entered
+    console.log(this)
+  }
+
+  componentDidMount() {
+    console.log('Mounted!!')
+  }
+
   render() {
     return (
-      <form className='store-selector'>
+      <form className='store-selector' onSubmit={this.goToStore}>
         { /*comment example it must exist within the returned element or fragment*/}
         <h2>Please Enter A Store</h2>
-        <input type='text' required placeholder='Store Name' defaultValue={getFunName()}/>
+        <input type='text'
+               ref={this.myInput}
+               required
+               placeholder='Store Name'
+               defaultValue={getFunName()}/>
         <button type='submit'>Vist Store &rarr;</button>
       </form>
     )
